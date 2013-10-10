fe74b58b29f79639ed0252e6f20aa73555020e91
Ignacio Cañón <nachete.canon@gmail.com>
2013-10-10 23:35:10 +0200

    homework 0 ok!

--- homeworks/hw0_1.rb
+++ homeworks/hw0_1.rb
@@ -7,4 +7,22 @@ def sum(arr)
   total
 end
 
-puts sum(['a','[b]','[c]'])
+def max_2_sum(arr)
+  total=0
+  if arr.kind_of?(Array) && !arr.empty?
+    newarr=arr.to_a.clone
+    total+=newarr.sort!.pop
+    num2=newarr.pop if !newarr.empty?
+    total+=num2.nil? ? 0:num2
+  end
+  total
+end
+
+def sum_to_n?(arr,num)
+  total=0
+  if arr.kind_of?(Array) && !arr.empty?
+    arr.to_a.permutation(2).to_a.each { |a,b| total=a+b if a+b==num }
+  end
+  total==num
+end
+
