5e50835988f9288b5f4f7ba88817bcae7169a472
Ignacio Cañón <nachete.canon@gmail.com>
2013-10-25 21:12:09 +0200

    resueltos todos

--- homeworks/hw1.5/lib/oracle_of_bacon.rb
+++ homeworks/hw1.5/lib/oracle_of_bacon.rb
@@ -38,16 +38,13 @@ class OracleOfBacon
     rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
       Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError,
       Net::ProtocolError => e
-      # convert all of these into a generic OracleOfBacon::NetworkError,
-      #  but keep the original error message
-      # your code here
+      raise NetworkError.new(e.message)
     end
-    # your code here: create the OracleOfBacon::Response object
+    Response.new(xml)
   end
 
   def make_uri_from_arguments
-    # your code here: set the @uri attribute to properly-escaped URI
-    #   constructed from the @from, @to, @api_key arguments
+    @uri="http://oracleofbacon.org/cgi-bin/xml?p=#{CGI::escape(@api_key)}&a=#{CGI::escape(@from)}&b=#{CGI::escape(@to)}"
   end
       
   class Response
